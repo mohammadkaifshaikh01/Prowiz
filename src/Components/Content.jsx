44694333c99f27feaaf83127c7874b07fbc5b496
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-02-14 11:45:53 +0530

    ok

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,40 +1,40 @@
-import { ChartBarIcon, DocumentTextIcon, CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/solid';
+const data = [
+  {
+    icons: "https://i.ibb.co/Z6SLz1dN/Frame-9.png",
+    name: "Items Processed",
+    number: 3200,
+  },
+  {
+    icons: "https://i.ibb.co/7xxSQBz1/Frame-9-1.png",
+    name: "Items Passed",
+    number: 1050,
+  },
+  {
+    icons: "https://i.ibb.co/7xxSQBz1/Frame-9-1.png",
+    name: "Items Rejected",
+    number: 18,
+  },
+  {
+    icons: "https://i.ibb.co/7xxSQBz1/Frame-9-1.png",
+    name: "Efficiency (Units/Minute)",
+    number: 125.75,
+  },
+];
+
 const States = () => {
-
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 mb-8">
-    <div className="bg-white p-6 rounded-lg shadow">
-      <div className="flex items-center space-x-2 mb-4">
-        <DocumentTextIcon className="h-6 w-6 text-blue-600" />
-        <h3 className="text-gray-600">Units Processed</h3>
-      </div>
-      <p className="text-3xl font-bold">2,575</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
+      {data.map((e, index) => (
+        <div
+          key={index}
+          className="border border-gray-300 rounded-xl p-5 flex flex-col items-left text-left bg-white shadow-md"
+        >
+          <img src={e.icons} alt={e.name} className="w-11 h-11 object-contain mb-3" />
+          <p className="text-lg font-semibold">{e.name}</p>
+          <p className="text-gray-500 text-xl font-bold">{e.number}</p>
+        </div>
+      ))}
     </div>
-    <div className="bg-white p-6 rounded-lg shadow">
-      <div className="flex items-center space-x-2 mb-4">
-        <CheckCircleIcon className="h-6 w-6 text-blue-600" />
-        <h3 className="text-gray-600">Units Passed</h3>
-      </div>
-      <p className="text-3xl font-bold">814</p>
-    </div>
-    <div className="bg-white p-6 rounded-lg shadow">
-      <div className="flex items-center space-x-2 mb-4">
-        <XCircleIcon className="h-6 w-6 text-blue-600" />
-        <h3 className="text-gray-600">Units Rejected</h3>
-      </div>
-      <p className="text-3xl font-bold">12</p>
-    </div>
-    <div className="bg-white p-6 rounded-lg shadow">
-      <div className="flex items-center space-x-2 mb-4">
-        <ChartBarIcon className="h-6 w-6 text-blue-600" />
-        <h3 className="text-gray-600">Throughput (Units/Minute)</h3>
-      </div>
-      <p className="text-3xl font-bold">110.41</p>
-    </div>
-  </div>
-
-
   );
 };
 
