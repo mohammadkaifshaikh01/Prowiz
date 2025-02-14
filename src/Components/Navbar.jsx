575c8f6e9972b19f2590a57bbbd4a0766f1bad67
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-02-14 12:24:36 +0530

    ok

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,16 @@ const Navbar = ({ toggleSidebar }) => {
         <button onClick={toggleSidebar} className="md:hidden text-gray-700">
           <FiMenu size={24} />
         </button>
-        <p>KPI Tracker</p>
+        <p className="text-xl font-semibold">KPI Tracker</p>
+      </div>
+
+      
+      <div className="flex items-center gap-4">
+        <img
+          src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" 
+          alt="User"
+          className="w-10 h-10 rounded-full border border-gray-300"
+        />
       </div>
     </div>
   );
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -6,7 +6,18 @@ import { FiX } from "react-icons/fi";
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <>
+      
       <div className="hidden md:flex flex-col bg-white shadow-md full-h-screen w-64 p-6">
+      
+        <div className="flex items-center justify-center mb-6">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/0996/ab83/16c447b5c9b902c14889d917f4e93550?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DcWpkvJlgVsrngvWevtvWo4j1HapeM5GYR6Mv4poc3zND28pDnobbviZ-fAdhmjID-mIxuqwA54Nk1xqVg01GRA8Aj23u7hpJZYIhe1P7Rf9~MxeZTe0IkTF8bVryc8zpJKZIgqxI~V4X~EMSFWHGboneZgzLySF5uQRSLmEYWrZH9EOKJHQ0Fyn7fvCu84avMW1SLSoTF2z-LFnXxeTKS9aBqCiTQ9sr7P4ucgT1ONopt3gVHMLV1Ppb3YlIopt2hiAiTGyOaWf9~QBqw8kdxSMzF8C0BpQrcr~6rwx~J21KO8~RsBTB9BvxFYTLjjm-6CJHbGxDf3Gc2VlWk4mng__" 
+            alt="Logo"
+            className="h-15"
+          />
+        </div>
+
+    
         <nav className="w-full">
           <ul className="space-y-4">
             <li className="flex items-center gap-3 text-gray-700 hover:text-blue-500 hover:bg-gray-100 p-3 rounded-md cursor-pointer">
@@ -25,6 +36,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         </nav>
       </div>
 
+      
       <div
         className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg p-6 z-40 transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
@@ -33,7 +45,18 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         <button className="absolute top-4 right-4 text-gray-700" onClick={toggleSidebar}>
           <FiX size={28} />
         </button>
-        <nav className="mt-12">
+
+      
+        <div className="flex items-center justify-center mb-6 mt-6">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/0996/ab83/16c447b5c9b902c14889d917f4e93550?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DcWpkvJlgVsrngvWevtvWo4j1HapeM5GYR6Mv4poc3zND28pDnobbviZ-fAdhmjID-mIxuqwA54Nk1xqVg01GRA8Aj23u7hpJZYIhe1P7Rf9~MxeZTe0IkTF8bVryc8zpJKZIgqxI~V4X~EMSFWHGboneZgzLySF5uQRSLmEYWrZH9EOKJHQ0Fyn7fvCu84avMW1SLSoTF2z-LFnXxeTKS9aBqCiTQ9sr7P4ucgT1ONopt3gVHMLV1Ppb3YlIopt2hiAiTGyOaWf9~QBqw8kdxSMzF8C0BpQrcr~6rwx~J21KO8~RsBTB9BvxFYTLjjm-6CJHbGxDf3Gc2VlWk4mng__" 
+            alt="Logo"
+            className="h-10"
+          />
+        </div>
+
+      
+        <nav className="mt-6">
           <ul className="space-y-6">
             <li className="flex items-center gap-3 text-gray-700 hover:text-blue-500 p-3 rounded-md cursor-pointer">
               <RxDashboard size={22} />
